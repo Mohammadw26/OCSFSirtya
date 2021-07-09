e20e9ea4869cc6bfbc67f851662e80ade5dc1d2f
Mohammadw26 <82311533+Mohammadw26@users.noreply.github.com>
2021-07-09 06:43:17 +0300

    Add files via upload

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/ContentManager.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/ContentManager.java
@@ -16,27 +16,14 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "content_managers")
 public class ContentManager extends Worker {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -7646477538037466098L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private List<Refund> refunds;
+	
 	
 	public ContentManager() {}
 	
-	public ContentManager (List <Refund> _refunds){
-		this.refunds = _refunds;
-	}
 	
-	public List <Refund> getRefunds(){
-		return this.refunds;
-	}
-	
-	public void setRefunds(List <Refund> _refunds){
-		this.refunds = _refunds;
-	}
 }
